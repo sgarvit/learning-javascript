7deece231b4bc37b930b31fc0645fc74d5492d93
Garvit Sharma <123792489+sgarvit@users.noreply.github.com>
2024-05-08 04:50:40 +0000

    example code

---
+++ 01_basics/variables.js
@@ -0,0 +1,5 @@
+// uses of let, const. var
+// perfer not use to var because they have an issue of block scope and many more .
+let salary = 43000;
+const employeeName = "Garvit Sharma";
+console.log("Hello "+employeeName + " This is your salary :" +salary);
