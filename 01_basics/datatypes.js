6974566abb8460c461c20bd21ab4cc9152faba0f
Garvit Sharma <123792489+sgarvit@users.noreply.github.com>
2024-05-08 05:03:40 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,11 @@
+// uses of datatypes
+let fullName = "Garvit Sharma"; // string datatype
+let age  = 20; // integer datatype
+let isLoggedIn = true; //boolean datatype
+
+// check variable type using this => typeof
+console.log(typeof age);
+console.log(typeof fullName);
+console.log(typeof isLoggedIn);
+console.log(typeof null);
+console.log(typeof undefined);
