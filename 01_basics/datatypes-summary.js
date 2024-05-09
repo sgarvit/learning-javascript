5cc90290ce6694550c463588956f738f7dd1e68a
Garvit Sharma <123792489+sgarvit@users.noreply.github.com>
2024-05-09 04:26:58 +0000

    datatypes-summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+// there are 2 types of datatypes 
+// 1. primitives datatypes
+    // there are 7 types in primitives 
+        // [String | Number | Boolean | null | undefined | Symbol | BigInt]
+        // Symbol - it basically used in advance js when we work with front end development then we want to make some component unique then we will use this datatype
+
+
+// 1. Non-primitives datatypes (This is also Known as Reference Datatypes)
+    // There are 3 types of Non-primitives datatypes 
+        // [Array | Objects | Functions]
+
+// Array example
+ let heros = ["Batman", "Superman", "Iron Man", "Captin America", "Hulk", "Docter Stange", "Mysterious", "Harry Porter"];
+
+//  objects
+let myBio = {
+    name: "Garvit Sharma",
+    age: 20,
+    qualifications: "BCA",
+    collegeName: "Mohyal Educational & Research Technology of Institute",
+}
+
+// function
+const additionFunction = function () {
+    console.log("Hello World!! This is my first small function");
+}
