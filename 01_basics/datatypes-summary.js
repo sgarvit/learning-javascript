43320dd88f1d9d4762f0f293bdcb9e2fd91c4ea2
Garvit Sharma <123792489+sgarvit@users.noreply.github.com>
2024-05-10 06:30:06 +0000

    stack & heap memory

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -24,3 +24,29 @@ let myBio = {
 const additionFunction = function () {
     console.log("Hello World!! This is my first small function");
 }
+
+// learn about memories
+// there are 2 types of memory 
+    // heap and stack memory
+
+// stack memory uses only in primitive data = iss stack memory ka data jab bhi change ya update hota hai toh uski data copy m hota hai original data pr koi imapct ni hota
+// example
+let user_name = "Garvit Sharma";
+let role = user_name;
+// console.log(role);
+role = "Web developer";
+// console.log(role);
+// console.log(user_name);
+// heap memory uses only in non primitive data
+// heap memory m jo data hota hai usme uss data ka reference milta hai aur uss reference se data ki original value change ya update hoti hai
+// example
+let employeeOne = {
+    employeeName : "Garvit Sharma",
+    Age : 20
+}
+
+let employeeTwo = employeeOne;
+employeeTwo.Age = 22;
+console.log(employeeOne.Age);
+console.log(employeeTwo.Age);
+
